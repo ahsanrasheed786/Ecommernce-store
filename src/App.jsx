e097c9fcdd0c168cfd65d7ce7a302d7738bb11f3
ahsanrasheed786 <rasheedahsan786@gmail.com>
2024-07-07 18:05:55 +0500

    empty

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-300 text-green-400'>
+        apple
+       </div>
+    </>
+  )
+}
+
+export default App
